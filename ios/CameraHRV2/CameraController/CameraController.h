635364a0441dddfc6320b442ddac76848217ed10
Tyler Davis <tylerdavis@berkeley.edu>
2018-01-18 21:07:45 -0800

    react native upgrade

--- ios/CameraHRV2/CameraController/CameraController.h
+++ ios/CameraHRV2/CameraController/CameraController.h
@@ -9,9 +9,8 @@
 
 #import <Foundation/Foundation.h>
 #import <GLKit/GLKit.h>
-#import <RCTBridgeModule.h>
-#import "RCTEventEmitter.h"
-
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
 @interface CameraController : RCTEventEmitter <RCTBridgeModule>
 
